a66e228288ed175e2428bea094d6ac265eb9ea04
GImDX <121179513+GImDX@users.noreply.github.com>
2025-05-28 10:43:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 cd /home/pi/oled
-g++ -g oled.cpp ssd1306_i2c.cpp ./MAX17043/MAX17043.cpp -o oled -lwiringPi -rdynamic
+g++ -g oled.cpp ssd1306_i2c.c ./MAX17043/MAX17043.cpp -o oled -lwiringPi -rdynamic
